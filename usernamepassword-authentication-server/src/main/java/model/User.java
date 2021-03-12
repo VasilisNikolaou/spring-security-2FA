32bf69ed8b443574254a070bc96245d6a9574d9e
Vasilis Nikolaou <basilisnik16@gmail.com>
2021-03-12 19:28:18 +0200

    Added entity model class

---
+++ usernamepassword-authentication-server/src/main/java/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+}
